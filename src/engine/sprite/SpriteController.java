3f2169735ff687be99b3ef1b3d72556c0670d2c0
Cowthulhu <david.ca1226@yahoo.com>
2016-02-08 14:28:32 -0700

    Got location correct
    Should be working now

--- src/engine/sprite/SpriteController.java
+++ src/engine/sprite/SpriteController.java
@@ -1,7 +1,7 @@
 package engine.sprite;
 
 public class SpriteController {
-	final static String[] locations = {"/16x16ShipSprites.png"};
+	final static String[] locations = {"res/16x16ShipSprites.png"};
 	final static int[] dimensions = {16};
 	static Spritesheet[] sheets;
 	public static void init() {
